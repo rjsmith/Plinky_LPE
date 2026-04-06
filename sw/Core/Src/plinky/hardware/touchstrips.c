412105396b4f3c88a57972abf8253ca4aba22931
RJ-Eckie <121803687+RJ-Eckie@users.noreply.github.com>
2026-04-06 22:50:29 +0200

    Refactor: Touchstrips encapsulation

--- sw/Core/Src/plinky/hardware/touchstrips.c
+++ sw/Core/Src/plinky/hardware/touchstrips.c
@@ -12,8 +12,8 @@ extern TSC_HandleTypeDef htsc;
 #define TOUCH_THRESHOLD 1000
 #define TOUCH_STABILITY_TICKS 9
 
-u8 touch_frame = 0; // frame counter for touch reading loop
-u16 strip_touched = 0;
+static u8 touch_frame = 0; // frame counter for touch reading loop
+static u16 strip_touched = 0;
 
 static TouchCalibData touch_calib_data[NUM_TOUCH_READINGS];
 static Touch touches[NUM_TOUCHSTRIPS][NUM_TOUCH_FRAMES];  // the touches
@@ -25,6 +25,14 @@ static u16 sensor_max[2 * NUM_TOUCH_READINGS];            // lifetime high
 
 static bool tsc_started = false;
 static u16 read_this_frame = 0; // has touch been read this touch_frame? bitmask
+
+u8 get_touch_frame(void) {
+	return touch_frame;
+}
+
+u16 get_strip_touched(void) {
+	return strip_touched;
+}
 
 // sensor macros
 
